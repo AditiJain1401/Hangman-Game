f8b61d09699bf229f437659d175a5ca75c6faf45
Aditi Jain <88366504+AditiJain1401@users.noreply.github.com>
2022-01-21 21:19:16 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,86 @@
-const wordsArr = [
-    { name: "Giraffe", hint: "tallest animal" },
-    { name: "Kiran Bedi", hint: "first woman I.P.S officer" },
-    { name: "Washington D.C.", hint: "capital city of U.S.A" },
-    { name: "rupee", hint: "a currency" }
-]
-let randomWordToGuess = null;
+   const alphabets = document.querySelector(".alphabets");
+   const gameStatus = document.querySelector("#status");
+   const wordsAndHints = [
+       { name: "Giraffe", hint: "tallest animal" },
+       { name: "Kiran Bedi", hint: "first woman I.P.S officer" },
+       { name: "Washington D.C.", hint: "capital city of U.S.A" },
+       { name: "Rupee", hint: "a currency" }
+   ]
+   let randomWordToGuess = null;
+   let answer = []
+   let remainingLives = 5;
+   const guesses = [];
 
-function getRandomWord() {
-    let index = Math.floor(Math.random() * wordsArr.length)
-    randomWordToGuess = wordsArr[index];
-}
+   function getRandomWord() {
+       let index = Math.floor(Math.random() * wordsAndHints.length)
+       randomWordToGuess = wordsAndHints[index];
+       render();
+   }
 
-function displayAlphabets() {
-    const start = "a"
-    const end = "z"
-    let htmlCode = ``
-    for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
-        html += String.fromCharCode(i);
-    }
-}
+   function displayAlphabets() {
+       const start = "A"
+       const end = "Z"
+       let htmlCode = ``
+       for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
+           //console.log(i);
+           let letter = String.fromCharCode(i)
+           htmlCode += `<button class="alphabetBtn" id="${i}" onclick="checkGuess(${i})">${letter}</button>`;
+       }
+       alphabets.innerHTML = htmlCode;
+       getRandomWord();
+   }
 
-function render() {}
-displayAlphabets();
+   function render() {
+       const guessedLetters = []
+       const displayBlock = document.querySelector(".word");
+       displayBlock.innerHTML = ""
+       answer = randomWordToGuess.name.split("");
+       console.log(answer);
+       for (let letter of answer) {
+           let button = document.createElement("button");
+           button.setAttribute('id', `${letter}`)
+           button.setAttribute('class', 'wordBtn')
+           if (letter == ".") {
+               button.innerText = '.'
+           } else if (letter != " " && guesses.length != 0) {
+               guesses.forEach(element => {
+                   if (element == letter) {
+                       button.innerText = letter;
+                       guessedLetters.push(letter)
+                   }
+               })
+           } else {
+               button.innerText = '-'
+           }
+           displayBlock.appendChild(button);
+       }
+       checkWin(answer, guessedLetters)
+   }
+
+   function checkWin(answer, guessedLetters) {
+       let newArr = answer.filter((element) => element != " " && element != '.')
+       if (newArr.sort().toString() == guessedLetters.sort().toString()) {
+           console.log(newArr, guessedLetters)
+           gameStatus.innerText = "Congratulations! You Won"
+       }
+   }
+
+   function checkGuess(charCode) {
+       const btn = document.getElementById(charCode);
+       btn.setAttribute('class', 'disabled');
+
+       if (remainingLives <= 0) {
+           let letter = String.fromCharCode(charCode);
+           let result = answer.find((element) => element == letter || element == letter.toLowerCase());
+           if (result != undefined) {
+               guesses.push(result);
+           } else {
+               remainingLives -= 1;
+               gameStatus.innerText = `You have ${remainingLives} remaining Lives`
+           }
+           render();
+       } else {
+           gameStatus.innerText = "Oops! Better Luck Next Time"
+       }
+   }
+   displayAlphabets();
